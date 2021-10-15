e13867dfc59c9e0c0d337987ce1fcef2873f546b
Gabriel Carollo <rimoldi98@gmail.com>
2021-10-15 11:20:41 -0300

    Initial Commit

---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 10f;
+
+    [ClientCallback]
+    private void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        CmdMove(move);
+    }
+
+    [Command]
+    private void CmdMove(Vector3 move)
+    {
+        transform.Translate(move * Time.deltaTime * moveSpeed);
+        RpcMove(transform.position);
+    }
+
+    [ClientRpc]
+    private void RpcMove(Vector3 position)
+    {
+        transform.position = position;
+    }
+}
